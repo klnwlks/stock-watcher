ebbd25d5de7439eee429a08425328449bdf0ccfe
klnwlks <95198127+klnwlks@users.noreply.github.com>
2022-11-04 23:07:59 +0800

    theming (wip)

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,7 @@
 import type { Component } from 'solid-js'; 
-import { createSignal, For, Show } from 'solid-js';
+import type { ITheme } from '../types'; 
+
+import { createSignal, For, Show, onMount } from 'solid-js';
 import { createStore } from 'solid-js/store'
 import { useKey } from '../KSContext'; 
 
@@ -7,14 +9,25 @@ import styles from './Settings.module.scss';
 
 const Settings: Component = () => {
   const [key, {add, remove}] = useKey();
-  const [style, setStyles] = createStore();
+  const [style, setStyles] = createStore<ITheme>();
   const [tempkey, setTK] = createSignal<string>('');
+  // add defaults later
+  let defTheme: ITheme;
+
+  onMount(() => {
+    if (localStorage.theme) setStyles(JSON.parse(localStorage.theme)) 
+    else setStyles(defTheme)
+  })
 
   const keySubmit = (e: SubmitEvent) => {
     e.preventDefault();
     add(tempkey());
   }
 
+  const saveTheme = (e: SubmitEvent) => {
+    e.preventDefault();
+    localStorage.theme = JSON.stringify(style);
+  }
 
   return (
     <div class={styles.settings}>
@@ -30,7 +43,7 @@ const Settings: Component = () => {
 	    <input type='text'
 	    value={tempkey()}
 	    onChange={(e) => setTK(e.currentTarget.value)}
-	    placeholder='input alphavantage api key'/>
+	    placeholder='input api key'/>
 	    <input type='submit' />
 	  </form>
 	</section>
@@ -38,7 +51,44 @@ const Settings: Component = () => {
 	<section>
 	  <h1>Theme</h1>
 
-	  <form>
+	  <form onSubmit={saveTheme}>
+	    <label>
+	      background
+	    </label>
+	    <input value={style.bg}
+	    onChange={(e) => setStyles('bg', e.currentTarget.value)}
+	    pattern='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$' />
+
+	    <label>
+	      sub bg
+	    </label>
+	    <input value={style.bg}
+	    onChange={(e) => setStyles('bg', e.currentTarget.value)}
+	    pattern='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$' />
+
+
+	    <label>
+	      main
+	    </label>
+	    <input value={style.fg1}
+	    onChange={(e) => setStyles('fg1', e.currentTarget.value)}
+	    pattern='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$' />
+
+	    <label>
+	      sub
+	    </label>
+	    <input value={style.fg2}
+	    onChange={(e) => setStyles('fg2', e.currentTarget.value)}
+	    pattern='^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$' />
+	    <input type='submit' value='save'/>
+
+	    <label>
+	      font
+	    </label>
+	    <input value={style.font}
+	    onChange={(e) => setStyles('font', e.currentTarget.value)}/>
+
+	    <input type='submit' value='save'/>
 	  </form>
 	</section>
     </div>

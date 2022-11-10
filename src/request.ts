daff7cc23de136e0e7c4a4af286066d2cb4354f8
klnwlks <95198127+klnwlks@users.noreply.github.com>
2022-11-10 15:53:00 +0800

    fixed request wrapper

--- src/request.ts
+++ src/request.ts
@@ -3,22 +3,16 @@ import axios from 'axios'
 export default async function APIreq(symbol: string, func: string, keys: string[]) {
   console.log(`called ${symbol} ${func}`);
   let baseURL = 'https://www.alphavantage.co/query';
-  let i = 0;
   let query = await axios.get(
     `${baseURL}?function=${func}&symbol=${symbol}&apikey=${keys[0]}`)
     .then(async (res) => {
-      while ('Note' in res.data && i < keys.length) {
-	i++;	
-	res.data = await axios.get(`${baseURL}?function=${func}&symbol=${symbol}&apikey=${keys[0]}`)
-			.then((rres) => {return rres.data});
-	if (i + 1 == keys.length) {
-	  console.log('restarting call')
-	  setTimeout(async () => {
-	    // doesnt work plsfix
-	    console.log('calling')
-	    res.data = await axios.get(`${baseURL}?function=${func}&symbol=${symbol}&apikey=${keys[0]}`)
-		    .then((rres) => {return rres.data});
-	  }, 60000);
+      console.log(res);
+      if ('Note' in res.data) {
+	res.data = () => {
+	  return new Promise(() => setTimeout(() =>{
+	    axios.get(`${baseURL}?function=${func}&symbol=${symbol}&apikey=${keys[0]}`)
+		.then((rres) => {return rres.data});
+	    }, 60000));
 	}
       }
 
